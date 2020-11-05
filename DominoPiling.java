b67f12bffaa9436bbc0c21361fda693067a741df
Sanjeev kumar <43263444+sanjeevkr07@users.noreply.github.com>
2020-11-06 02:16:29 +0530

    Add files via upload

---
+++ DominoPiling.java
@@ -0,0 +1,19 @@
+package codeForces_800;
+
+import java.util.Scanner;
+
+public class DominoPiling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//50A
+		Scanner sc=new Scanner(System.in);
+		int m=sc.nextInt();
+		int n=sc.nextInt();
+		System.out.println((int)(m*n)/2);
+		
+		sc.close();
+
+	}
+
+}
